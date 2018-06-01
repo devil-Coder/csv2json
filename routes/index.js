457ab7dc5f0fd32ec18973d17d54036a643334d6
devil-Coder <raj.chandra2016@vitstudent.ac.in>
2018-06-01 16:22:26 +0530

    added json file code

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,23 @@ var router = express.Router();
 const csv = require('csvtojson');
 const csvFilePath='public/CSV/Entities.csv';
 
+var fs = require('fs');
+
 try{
     csv()
         .fromFile(csvFilePath)
         .then((jsonObj)=>{
             var jsonObject = jsonObj;
-            console.log(jsonObj.length);
+            console.log(jsonObj);
+            var jsonData = JSON.stringify(jsonObj);
+
+            fs.writeFile('./public/JSON/myjsonfile.json', jsonData, 'utf8', function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(jsonData);
+                }
+            });
         })
 } catch(e){
     console.log(e);
